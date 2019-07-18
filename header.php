fcb12b4d6832cdefae93bac63adcb40c22c3eea7
ConnorNally <nallyconnor@gmail.com>
2019-07-18 19:17:42 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css"/>
+<body>
+
+  <ul class="flex-container2 header">
+    <li><a href="#s1">Connor</a></li>
+    <li><a href="#s2">About</a></li>
+    <li><a href="#s3">Work</a></li>
+    <li><a href="#s4">Contact</a></li>
+  </ul>
+</html>
---
+++ home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<?php include 'footer.php';
+      include 'header.php'
+      ?>
+<head>
+  <title>Connor Nally</title>
+</head>
+
+<body>
+
+<div class=a id= "s1">
+  <p>Connor Nally, currently looking to start a professional career.<p>
+
+  <h1><img src= "img/construction.gif"></h1>
+
+</div>
+
+<div class="about" id= "s2">
+  <p class="main">Based in Kent, I currently work in an admin possition, learning
+                  programing in my spare time.
+                  <br>
+  All knowledge and skills have been self taught,
+      using services like tree house and creating my own projects.
+      <br><br><br>
+    </p>
+  <div>
+      <p class="text">Javascript</p>
+       <p class="text">PHP</p>
+        <p class="text"> CSS </p>
+          <p class="text">HTML</p>
+  </div>
+
+</div>
+
+<div class="c" id= "s3">
+<div class="grid">
+    <div class="item">Does this website count as work experience</div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+</div>
+</div>
+<div class="d" id="s4">
+</div>
+</body>
+
+</html>
