7a38a9a0196be860a4a51c07281c0cc61bec540e
ConnorNally <nallyconnor@gmail.com>
2019-07-23 18:04:37 +0100

    Lots of small additions
    Started a script, need to add more interesting things to this.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function textAppear() {
+    console.log("Hello!");
+    document.querySelector("#work-item").style.display = "block";
+}
+
+function textGone() {
+    document.querySelector("#work-item").style.display = "none";
+}
