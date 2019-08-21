6743b0cd261631ef2577ff6e9d2287f83dae1afc
ConnorNally <nallyconnor@gmail.com>
2019-08-21 11:09:24 +0100

    Redesign!

--- scripts.js
+++ scripts.js
@@ -6,3 +6,24 @@ function textAppear() {
 function textGone() {
     document.querySelector("#work-item").style.display = "none";
 }
+
+let downloadButton = document.querySelector('.button');
+if(downloadButton) {    
+  downloadButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    /* Start loading process. */
+    downloadButton.classList.add('loading');
+    
+    /* Set delay before switching from loading to success. */
+    window.setTimeout(function() {
+      downloadButton.classList.remove('loading');
+      downloadButton.classList.add('success');
+    }, 3000);
+    
+    /* Reset animation. */
+    window.setTimeout(function() {
+      downloadButton.classList.remove('success');
+    }, 8000);
+  });
+};
