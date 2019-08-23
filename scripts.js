6c712bb19e31c6031640f42c95a4ec6f706b5ea9
ConnorNally <nallyconnor@gmail.com>
2019-08-23 17:41:01 +0100

    Small updates

--- scripts.js
+++ scripts.js
@@ -7,21 +7,6 @@ function textGone() {
     document.querySelector("#work-item").style.display = "none";
 }
 
-let downloadButton = document.querySelector('.button');
-if(downloadButton) {    
-  downloadButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    /* Start loading process. */
-    downloadButton.classList.add('loading');
-    
-    /* Set delay before switching from loading to success. */
-    window.setTimeout(function() {
-      downloadButton.classList.remove('loading');
-      downloadButton.classList.add('success');
-    }, 3000);
-    
-
 function gitHub(){
   window.location='https://github.com/ConnorNally';
 }
